8072cb4641312f129bf9d31616736339b3b29f9d
tomas-radic <tomas.radic@gmail.com>
2014-07-08 21:40:26 +0200

    migration corrected

--- db/migrate/20140624100637_change_year_of_birth_to_int.rb
+++ db/migrate/20140624100637_change_year_of_birth_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeYearOfBirthToInt < ActiveRecord::Migration
   def change
-    change_column :users, :year_of_birth, :integer
+    change_column :users, :year_of_birth, 'integer USING CAST(year_of_birth AS integer)'
   end
 end
