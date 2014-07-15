9e7ef66c5d74d2424485fdb25923f5fa221ed8e5
tomas-radic <tomas.radic@gmail.com>
2014-07-15 14:23:07 +0200

    Admins routed to users after sign-in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,8 @@ class SessionsController < ApplicationController
     
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or matches_path
+      default_path = user.admin? ? users_path : matches_path
+      redirect_back_or default_path
     else
       flash.now[:error] = 'Neplatná kombinácia emailu a hesla.' # Not quite right!
       render 'new'
