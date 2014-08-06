1c27d4c31bda1cadb2b80edd2340ea1952da0570
tomas-radic <tomas.radic@gmail.com>
2014-08-06 12:12:28 +0200

    Fixed today's events on homepage.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     @closest_match = nil
     @closest_training = nil
-    closest_events = Match.where('date_of_play > ?', Date.today)
+    closest_events = Match.where('date_of_play >= ?', Date.today)
 
     if closest_events
       @closest_match = (closest_events.where('category_id = ?', 1).order(date_of_play: :asc)).first
